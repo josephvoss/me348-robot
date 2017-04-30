4da75c8649251ca8c088052da53924fb65ac316e
josephvoss <josephvoss@utexas.edu>
2017-04-29 19:11:56 -0500

    Reducing tick count to take sensor data at

--- Drive_wrapper.cpp
+++ Drive_wrapper.cpp
@@ -38,55 +38,55 @@ Drive_wrapper::~Drive_wrapper()
 
 void Drive_wrapper::drive(int ticks)
 {
-    //Ask for sensor data every 30 ticks?
-         int distance = ticks;
-         int multiple_ticks = floor(distance/30);
-         int remainder = distance-multiple_ticks;
-         int x = 0; int y = 0; int l_count; int r_count;
-         for(int i=0; i<multiple_ticks; i++)
-         {
-             drive_goto(multiple_ticks, multiple_ticks); // blocking
-             
-             drive_getTicks(&l_count, &r_count); //Calc actual no. travelled
-             
-             //Logic to determine direction
-             //   0
-             // 3   1
-             //   2
-             
-             //ugly code block but at least it's reasonable
-             x = 0; y = 0;
-             if (orientation == 0) y = 1;
-             if (orientation == 1) x = 1;
-             if (orientation == 2) y = -1;
-             if (orientation == 3) x = -1;
-             cur_pos_y += l_count*y;
-             cur_pos_x += r_count*x;
-             
-             //Log?
-             //Set Sensor flag?
-         }
-         drive(remainder);
+    //Ask for sensor data every 15 ticks?
+    int distance = ticks;
+    int multiple_ticks = floor(distance/15);
+    int remainder = distance-multiple_ticks;
+    int x = 0; int y = 0; int l_count; int r_count;
+    for(int i=0; i<multiple_ticks; i++)
+    {
+        drive_goto(multiple_ticks, multiple_ticks); // blocking
+        
+        drive_getTicks(&l_count, &r_count); //Calc actual no. travelled
+        
+        //Logic to determine direction
+        //   0
+        // 3   1
+        //   2
+        
+        //ugly code block but at least it's reasonable
+        x = 0; y = 0;
+        if (orientation == 0) y = 1;
+        if (orientation == 1) x = 1;
+        if (orientation == 2) y = -1;
+        if (orientation == 3) x = -1;
+        cur_pos_y += l_count*y;
+        cur_pos_x += r_count*x;
+        
+        //Log?
+        //Set Sensor flag?
+    }
+    drive(remainder);
 }
 
 void Drive_wrapper::turn(int turnLeft, int turnRight)
 {
-        int l_count_d; int r_count_d; int l_count_e; int r_count_e;
-        
-        //Logic to take care of count amounts to turn each wheel
-        // and to update the orientation of the bot
-        if (turnLeft) { l_count_d = -25; r_count_d = 26;
-                        orientation = (orientation - 1) % 4;}
-        if (turnRight) { l_count_d = 26; r_count_d = -25;
-                         orientation = (orientation + 1) % 4;}
-        
-        //90 deg turn
-        drive_goto(l_count_d,r_count_d);
-        
-        //Overshoot protection (But only protects from 1?
-        drive_getTicks(&l_count_e, &r_count_e); //Calc actual no. travelled
-        l_count_d = l_count_d - l_count_e;
-        r_count_d = r_count_d - r_count_e;
-        drive_goto(l_count_d,r_count_d);
+    int l_count_d; int r_count_d; int l_count_e; int r_count_e;
+    
+    //Logic to take care of count amounts to turn each wheel
+    // and to update the orientation of the bot
+    if (turnLeft) { l_count_d = -25; r_count_d = 26;
+                    orientation = (orientation - 1) % 4;}
+    if (turnRight) { l_count_d = 26; r_count_d = -25;
+                     orientation = (orientation + 1) % 4;}
+    
+    //90 deg turn
+    drive_goto(l_count_d,r_count_d);
+    
+    //Overshoot protection (But only protects from 1?
+    drive_getTicks(&l_count_e, &r_count_e); //Calc actual no. travelled
+    l_count_d = l_count_d - l_count_e;
+    r_count_d = r_count_d - r_count_e;
+    drive_goto(l_count_d,r_count_d);
     
 }  
