12f74a7eafc8e5f5174267f76bec47e24fa195ad
Joseph Voss <brosephvossenator@gmail.com>
2017-05-10 18:41:04 -0500

    Updating main RoboMaze file with micheal's drive code

--- RoboMaze.cpp
+++ RoboMaze.cpp
@@ -1,7 +1,3 @@
-/*
-  Blank Simple Project.c
-  http://learn.parallax.com/propeller-c-tutorials 
-*/
 #include "simpletools.h"                      // Include simple tools
 #include "servo.h"
 #include "ping.h"
@@ -83,8 +79,6 @@ void modFF(int** ff, int* goal, int** walls)
     return;
 }
 
-
-
 void checkSignal()
 {
   int irLeft,irRight,sonarDis;
@@ -95,13 +89,13 @@ void checkSignal()
     irRight = input(2);
     sonarDis = ping_cm(17);    
     
-    if (sonarDis > 5 && irLeft == 0 && irRight == 0)
+    if (sonarDis > 20 && irLeft == 0 && irRight == 0)
     {
       //can only go straight
       check = 0;
       pause(300);
     }
-    else if (sonarDis <= 5 && irLeft == 0 && irRight == 0)
+    else if (sonarDis <= 20 && irLeft == 0 && irRight == 0)
     {
       //dead end, turn back
       check = 2;
@@ -124,23 +118,23 @@ void sensorDec()
     sonarDis = ping_cm(17);    
     
     // Decision based on sensor data
-    if (sonarDis > 5 && irLeft == 0 && irRight == 0) //forward
+    if (sonarDis > 20 && irLeft == 0 && irRight == 0) //forward
     {
       move = 1;
     }
-    else if (sonarDis <= 5 && irLeft == 1 && irRight == 0) //turn left
+    else if (sonarDis <= 20 && irLeft == 1 && irRight == 0) //turn left
     {
       move = 2;
     }
-    else if (sonarDis <= 5 && irLeft == 0 && irRight == 1) //turn right
+    else if (sonarDis <= 20 && irLeft == 0 && irRight == 1) //turn right
     {
       move=3;
     }
-    else if (sonarDis > 5 && irLeft == 1 && irRight == 0) //forward or left
+    else if (sonarDis > 20 && irLeft == 1 && irRight == 0) //forward or left
     {
       move=rand()%2+1;
     }
-    else if (sonarDis > 5 && irLeft == 0 && irRight == 1) //forward or right
+    else if (sonarDis > 20 && irLeft == 0 && irRight == 1) //forward or right
     {
       int a;
       a=rand()%2+1;
@@ -150,11 +144,11 @@ void sensorDec()
       }
       move=a;
     }
-    else if (sonarDis > 5 && irLeft == 1 && irRight == 1) //left or right
+    else if (sonarDis > 20 && irLeft == 1 && irRight == 1) //left or right
     {
       move=rand()%2+1;
     }         
-    else if (sonarDis <= 5 && irLeft == 0 && irRight == 0) //turn around
+    else if (sonarDis <= 20 && irLeft == 0 && irRight == 0) //turn around
     {
       move=4;
     }                                     
@@ -162,7 +156,9 @@ void sensorDec()
         
 void stepUp()
 {
-  drive_goto(1,1);
+  drive_setRampStep(2);
+  drive_goto(90,90);
+  
 }
 
 void turn()
@@ -174,12 +170,12 @@ void turn()
       break;
       
       case 2: //turn left
-      drive_goto(26,-25);
+      drive_goto(-26,25);
       pause(200);
       break;
       
       case 3: //turn right
-      drive_goto(-26,25);
+      drive_goto(26,-25);
       pause(200);
       break;
       
@@ -197,45 +193,48 @@ void selfOrient()
   d1 = 0;
   c2 = 100;
   d2 = 0;
-  for (a1=90;a1<=180;a1=+5)
+  for (a1=135;a1<=180;a1+=5)
   {
-     servo_angle(16,a1);
+     servo_angle(16,a1*10);
      pause(100);
-     b1 = ping_cm(3);
-     if (b1 < c1)
+     b1 = ping_cm(17);
+     if (b1 <= c1)
      {
-       d1 = 90-a1;
+       d1 = 180-a1;
        c1 = b1;
      }
    }
-   servo_angle(16,90);
-   pause(500);          
-  for (a2=90;a2<=0;a2=-5)
+   servo_angle(16,900);
+   pause(1000);   
+
+           
+  for (a2=45;a2>=0;a2-=5)
   {
-    servo_angle(16,a2);
+    servo_angle(16,a2*10);
     pause(100);
-    b2 = ping_cm(3);
-    if (b2 < c2)
+    b2 = ping_cm(17);
+    if (b2 <= c2)
     {
-      d2 = 90 - a2;
-      c2 = b1;
+      d2 = a2;
+      c2 = b2;
     }
   }
-  servo_angle(16,90);
-  pause(500);
-  if (d1 <= d2)
+    servo_angle(16,900);
+    if (d1 >= d2)
   {
     rotateTicks=d1*51/180;
-    drive_goto(-rotateTicks,rotateTicks);
-    pause(500);
-  }
-  else if (d1 > d2)
-  {
-    rotateTicks=d2*51/180;
     drive_goto(rotateTicks,-rotateTicks);
-    pause(500);
-  }
-}        
+    pause(500); 
+  
+} 
+  else if (d1 < d2)
+  {
+ rotateTicks=d2*51/180-4;
+  drive_goto(-rotateTicks,rotateTicks);
+  pause(500);  
+  }  
+} 
+       
 void directionUpdate() //North-0 East-1 South-2 West-3
 {
     switch (move){
@@ -338,16 +337,17 @@ void positionUpdate() //(x,y)
 //main loop
 int main()                                    // Main function
 {  
-direction=0;
-x=0;
-y=0;
-//drive_trimSet(0,0,0);
-while(1)
-{
-  stepUp();  
-  sensorDec();
-  turn();    
-  positionUpdate();
-  directionUpdate();
-}    
+  direction=0;
+  x=0;
+  y=0;
+  //drive_trimSet(0,0,0);
+  while(1)
+  {
+    selfOrient();
+    stepUp();  
+    sensorDec();
+    turn();    
+    positionUpdate();
+    directionUpdate();
+  }    
 }
